8ec65fe5b43dca623748bb405cfd78f0880dbb51
chedighribi <chady.ghribi.gc@gmail.com>
2025-06-02 09:40:36 +0200

    feat: init project with dependencies

---
+++ src/main/java/fr/eni/cave_a_vin/CaveAVinApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.cave_a_vin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaveAVinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaveAVinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/cave_a_vin/CaveAVinApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.cave_a_vin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaveAVinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
