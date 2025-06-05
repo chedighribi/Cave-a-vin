cd1e754e05898ecc7e8d8c2a501a11def9faacfe
chedighribi <chady.ghribi.gc@gmail.com>
2025-06-05 14:27:51 +0200

    feat: panier and test association

---
+++ src/main/java/fr/eni/cave/bo/client/LignePanier.java
@@ -0,0 +1,26 @@
+package fr.eni.cave.bo.client;
+import jakarta.persistence.*;
+import lombok.*;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = { "id" })
+@ToString
+@Builder
+
+@Entity
+@Table(name = "CAV_LINE")
+public class LignePanier {
+	
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name = "LINE_ID")
+private Integer id;
+
+@Column(name = "QUANTITY")
+private int quantite;
+
+@Column(name = "PRICE", precision = 2)
+private float prix;
+}
---
+++ src/main/java/fr/eni/cave/bo/client/Panier.java
@@ -0,0 +1,37 @@
+package fr.eni.cave.bo.client;
+
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = { "id" })
+@ToString
+
+@Builder
+@Entity
+@Table(name = "CAV_SHOPPING_CART")
+public class Panier {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name = "SHOPPING_CART_ID")
+private Integer id;
+
+@Column(name = "ORDER_NUMBER", length = 200)
+private String numCommande;
+
+@Column(name = "TOTAL_PRICE", precision=2)
+private float prixTotal;
+
+@Column(name = "PAID")
+private boolean paye;
+
+@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+@JoinColumn(name="SHOPPING_CART_ID")
+private @Builder.Default List<LignePanier> lignesPanier = new ArrayList<>();
+
+}
